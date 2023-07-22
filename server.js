94c17c16138f68b54fca9367f0b81b06b022c9a8
eabelloc <eabelloc.dev@gmail.com>
2023-07-22 15:59:50 +0200

    Little details 5

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// server.js
+/*SERVER*/
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' folder
+/*Serve static files from the 'public' folder*/
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('A user connected.');
 
-  // Listen for waveform data from the clients and broadcast it to all connected clients
+  /*Listen for waveform data from the clients and broadcast it to all connected clients*/
   socket.on('waveformData', (data) => {
     io.emit('waveformData', data);
   });
